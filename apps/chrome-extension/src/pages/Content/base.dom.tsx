3b09db942f7113bc21251bf4689c8da3225bc11b
rpidanny <abhishekmaharjan1993@gmail.com>
2023-10-27 05:38:20 +0200

    chore: wip

--- apps/chrome-extension/src/pages/Content/base.dom.tsx
+++ apps/chrome-extension/src/pages/Content/base.dom.tsx
@@ -1,9 +1,13 @@
 import { IPrompt } from '@rpidanny/llm-prompt-templates';
+import mixpanel from 'mixpanel-browser';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { Metrics } from './metrics';
 import { groupedPrompts } from './prompts';
 import PromptsView from './PromptsView';
+
+mixpanel.init('1eb7876e8cc0adf4a46631b5ba85b4d5');
 
 export abstract class BaseDom {
   protected abstract name: string;
@@ -22,11 +26,18 @@ export abstract class BaseDom {
 
   init() {
     console.log(`Initializing ${this.name} DOM`);
+    mixpanel.identify('local');
     this.promptsView = this.createPromptsElement();
     this.addTriggers();
   }
 
   private handlePromptSelected(prompt: IPrompt): void {
+    console.log('Saving prompt selected metric');
+    mixpanel.track(Metrics.PromptSelected, {
+      promptName: prompt.name,
+      promptCategory: prompt.category,
+    });
+
     this.isPromptsViewOpen = false;
     this.usePrompt(prompt);
     this.hidePrompts();
@@ -51,6 +62,8 @@ export abstract class BaseDom {
   }
 
   protected async showPrompts() {
+    mixpanel.track(Metrics.PromptListShown);
+
     this.isPromptsViewOpen = true;
     await this.render();
     this.promptsView.focus();
